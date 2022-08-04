78385e4bd3150d7027b1df6d5744a71faf441782
Andrea Fang <arhughes43@gmail.com>
2022-08-04 16:50:56 -0700

    before change

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import ContactForm from './components/Contact';
+import Portfolio from './components/Portfolio';
 
 function App() {
+  // edit these whiule still rendering images??
+  const [categories] = useState([
+    {
+      name: 'About Me', description: 'Description'
+    },
+    { name: 'Portfolio', description: 'Description' },
+    { name: 'Contact', description: 'Description' },
+    { name: 'Resume', description: 'Description' }
+  ]); 
+  
+  // TODO: change to aboutSelected and default true
+  const [contactSelected, setContactSelected] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* 
+        WHEN I view the header
+        THEN I am presented with the developer's name and navigation 
+        with titles corresponding to different sections of the portfolio
+         */}
+        <Nav
+          categories = {categories}
+          setCurrentCategory = {setCurrentCategory}
+          currentCategory = {currentCategory}
+          contactSelected = {contactSelected}
+          setContactSelected = {setContactSelected} 
+        ></Nav>
+
       <main>
+        <About></About>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+        )}
+      </main>
+      <main className='App-main'>
         {/* section for content */}
       </main>
-      <footer>
+      <footer className='App-footer'>
         {/* text or icon links to the developer’s GitHub and 
         LinkedIn profiles, and their profile on a third 
         platform (Stack Overflow, Twitter)  */}
-        <link href='https://github.com/ah-fang'>GitHub</link>
-        <link href='https://www.linkedin.com/in/andrea-hughes-3b1b94177/'>LinkedIn</link>
-        <link href='https://twitter.com/AndreaH85273363'>Twitter</link>
+        <a href='https://github.com/ah-fang' className='App-link'>GitHub</a>
+        <a href='https://www.linkedin.com/in/andrea-hughes-3b1b94177/' className='App-link'>LinkedIn</a>
+        <a href='https://twitter.com/AndreaH85273363' className='App-link'>Twitter</a>
       </footer>
     </div>
   );
